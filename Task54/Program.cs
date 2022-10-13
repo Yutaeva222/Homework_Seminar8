75a5af5dbd9a27b8f98658f12796c23619909ecc
Maria Yutaeva <mariya1982@yandex.ru>
2022-10-13 11:02:21 +0300

    задача54

---
+++ Task54/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит 
+// по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+Console.WriteLine("Укажите размерность m вашего массива:  ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Укажите размерность n вашего массива:  ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] numbers = new int[rows, columns];
+FillArray(numbers);
+PrintArray(numbers);
+OrderArrayLines(numbers);
+Console.WriteLine("Массив с упорядоченными по убыванию элементами: ");
+PrintArray(numbers);
+
+void FillArray(int[,] numbers)
+{
+    int rows = numbers.GetLength(0);
+    int columns = numbers.GetLength(1);
+    Random random = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            numbers[i, j] = random.Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] numbers)
+{
+    int rows = numbers.GetLength(0);
+    int columns = numbers.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write(numbers[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+void OrderArrayLines(int[,] numbers)
+{
+  for (int i = 0; i < numbers.GetLength(0); i++)
+  {
+    for (int j = 0; j < numbers.GetLength(1); j++)
+    {
+      for (int k = 0; k < numbers.GetLength(1) - 1; k++)
+      {
+        if (numbers[i, k] < numbers[i, k + 1])
+        {
+          int temp = numbers[i, k + 1];
+          numbers[i, k + 1] = numbers[i, k];
+          numbers[i, k] = temp;
+        }
+      }
+    }
+  }
+}
